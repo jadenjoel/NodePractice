cbea036cbafb48400c8c1b4045688ae75f95d21a
jadenjoel <mailtojadenjoel76@gmail.com>
2022-08-04 00:10:41 +0100

    completely removed middleware as affected deploy again!

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
-app.get("/secret", auth(config), (req, res) => {
+app.get("/secret", (req, res) => {
   res.render("secret", {
     title: "Secret",
     isAuthenticated: req.oidc.isAuthenticated(),
